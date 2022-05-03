7014e00abb0e1820bb2c0d2915e9a8e093b0a104
Arnaud Brunet <gronono@users.noreply.github.com>
2022-05-03 15:11:17 +1100

    extraction du traitement du resultset

--- src/main/java/org/example/gl/ces/dao/taux/TauxDao.java
+++ src/main/java/org/example/gl/ces/dao/taux/TauxDao.java
@@ -2,32 +2,37 @@ package org.example.gl.ces.dao.taux;
 
 import org.example.gl.ces.dao.connection.ConnectionHolder;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class TauxDao {
 
     public TauxEntity findAll() {
-        var taux = new TauxEntity();
         var connexion = ConnectionHolder.INSTANCE.getConnection();
         try (var statement = connexion.createStatement()) {
             try (var result = statement.executeQuery("select * from taux")) {
-                while (result.next()) {
-                    var nom = result.getString(1);
-                    var value = result.getDouble(2);
-                    switch (nom) {
-                        case "CES":
-                            taux.setTauxCes(value);
-                            break;
-                        case "CONGES":
-                            taux.setTauxConges(value);
-                            break;
-                        default:
-                            throw new IllegalStateException("Taux non supporté : nom=" + nom + ", valeur=" + value);
-                    }
-                }
+               return processResultSet(result);
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private TauxEntity processResultSet(ResultSet result) throws SQLException {
+        var taux = new TauxEntity();
+        while (result.next()) {
+            var nom = result.getString(1);
+            var value = result.getDouble(2);
+            switch (nom) {
+                case "CES":
+                    taux.setTauxCes(value);
+                    break;
+                case "CONGES":
+                    taux.setTauxConges(value);
+                    break;
+                default:
+                    throw new IllegalStateException("Taux non supporté : nom=" + nom + ", valeur=" + value);
+            }
         }
         return taux;
     }
